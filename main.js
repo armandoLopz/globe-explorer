d0848dbbf0388355089b296cdc272095f2c83129
Yuyi <alexandrayuyi@gmail.com>
2023-10-13 18:40:39 -0400

    pin moved

--- main.js
+++ main.js
@@ -14,16 +14,7 @@ var texto = document.querySelector("p")
 var fotomostrar = document.querySelector("img")
 // FUNCION PARA OCULTAR EL CONTENIDO
 function ocultar() {
-    pin.style.display = "none";
-    pin.style.top = "220px";
-    pin.style.left = "285px";
-    var h = plano.animate(
-        { transform: "translateX(0px)" },
-        { duration: 800, fill: "forwards" }
-    )
-    h.onfinish = function () {
-        pin.style.display = "block";
-    };
+
     document.getElementById("campo").style.display = "none"
     document.getElementById("bandera").style.display = "none"
 
@@ -33,8 +24,8 @@ function ocultar() {
 
 function mostrarmexico() {
     pin.style.display = "none";
-    pin.style.top = "185px";
-    pin.style.left = "200px";
+    pin.style.top = "160px";
+    pin.style.left = "170px";
     var h = plano.animate(
         { transform: "translateX(0px)" },
         { duration: 800, fill: "forwards" }
@@ -47,31 +38,19 @@ function mostrarmexico() {
     titulo.innerHTML = "México"
 
     texto = document.querySelector("p")
-    texto.innerHTML =  `Presidente: Andrés Manuel López Obrador. <br> <br>
-    Capital: Ciudad de México. <br> <br>
+    texto.innerHTML = `Presidente: Andrés Manuel López Obrador. <br> <br>
     Platos típicos: Tacos al pastor, Mole poblano, Pozole. <br> <br>
     Lugares turísticos: Chichén Itzá, Ciudad de México, Cancún <br> <br>
-    Población: 126,7 millones de personas. <br> <br>
-    Extensión territorial: 1,973 millones km². <br> <br>
     Producto Interno Bruto (PIB): 1.05 billones de dólares.`
     fotomostrar = document.querySelector("img")
     fotomostrar.setAttribute("src", "mexico.svg")
 
-    campo.animate(
-        {
-          opacity: [0, 1]
-        },
-        2100,
-      );
-
     document.getElementById("regresar").style.display = "block"
-
     document.getElementById("campo").style.display = "block"
     document.getElementById("bandera").style.display = "block"
     document.getElementById("textoinicio").style.display = "none"
 
 }
-
 
 //ELEMENTOS PARA MOSTRAR EL CONTENIDO DE BRASIL
 
@@ -81,8 +60,8 @@ botonmostrar.addEventListener("click", mostrarbrasil)
 
 function mostrarbrasil() {
     pin.style.display = "none";
-    pin.style.top = "270px";
-    pin.style.left = "365px";
+    pin.style.top = "260px";
+    pin.style.left = "350px";
     var h = plano.animate(
         { transform: "translateX(0px)" },
         { duration: 800, fill: "forwards" }
@@ -91,26 +70,16 @@ function mostrarbrasil() {
         pin.style.display = "block";
     };
 
-
     titulo.innerHTML = "Brasil"
 
     texto = document.querySelector("p")
     texto.innerHTML = `Presidente: Luiz Inácio Lula da Silva. <br> <br>
-    Capital: Brasilia. <br> <br>
     Platos típicos: Feijoada, Acarajé, Pan de Queso. <br><br>
     Lugares turísticos: Cristo Redentor, Cataratas del Iguazú, Pan de Azúcar.<br><br>
-    Población: 214,3 millones de personas. <br> <br>
-    Extensión territorial: 8,51 millones km². <br> <br>
     Producto interno bruto (PIB): 1.924.134 M$`
 
     fotomostrar.setAttribute("src", "brasil.png")
 
-    campo.animate(
-        {
-          opacity: [0, 1]
-        },
-        2100,
-      );
 
     document.getElementById("campo").style.display = "block"
     document.getElementById("bandera").style.display = "block"
@@ -127,8 +96,8 @@ botonmostrar.addEventListener("click", mostraresp)
 
 function mostraresp() {
     pin.style.display = "none";
-    pin.style.top = "125px";
-    pin.style.left = "115px";
+    pin.style.top = "100px";
+    pin.style.left = "97px";
     var h = plano.animate(
         { transform: "translateX(-380px)" },
         { duration: 800, fill: "forwards" }
@@ -138,32 +107,20 @@ function mostraresp() {
     };
     titulo.innerHTML = "España"
 
+
     texto = document.querySelector("p")
     texto.innerHTML = `Presidente: Pedro Sánchez.<br><br>
-    Capital: Madrid. <br> <br>
     Platos típicos: Las tapas, Paella, Tortilla de papa.<br><br>
     Lugares turísticos: Isla de Tabarca, Formentera, Basílica de la Sagrada Família.<br><br>
-    Población: 47,42 millones de personas. <br> <br>
-    Extensión territorial: 506.030 km². <br> <br>
     Producto interno bruto (PIB): 1.400,52M$`
     fotomostrar = document.querySelector("img")
-    
-    
+
     fotomostrar.setAttribute("src", "espana.png")
-   
-        campo.animate(
-            {
-              opacity: [0, 1]
-            },
-            2100,
-          );
 
     document.getElementById("campo").style.display = "block"
     document.getElementById("bandera").style.display = "block"
     document.getElementById("regresar").style.display = "block"
     document.getElementById("textoinicio").style.display = "none"
-        
-    
 }
 
 //ELEMENTOS PARA MOSTRAR EL CONTENIDO DE ALEMANIA
@@ -174,8 +131,8 @@ botonmostrar.addEventListener("click", mostrarreinounido)
 
 function mostrarreinounido() {
     pin.style.display = "none";
-    pin.style.top = "80px";
-    pin.style.left = "115px";
+    pin.style.top = "60px";
+    pin.style.left = "95px";
     var h = plano.animate(
         { transform: "translateX(-380px)" },
         { duration: 800, fill: "forwards" }
@@ -187,21 +144,11 @@ function mostrarreinounido() {
 
     texto = document.querySelector("p")
     texto.innerHTML = `Presidente: Boris Johnson.<br><br>
-    Capital: Londres. <br> <br>
     Platos típicos: Fish & chips, cream tea, falafel.<br><br>
     Lugares turísticos: Torre de Londres, Museo Británico, Puente de la Torre.<br><br>
-    Población: 67,33 millones de personas. <br> <br>
-    Extensión territorial: 243.610 km². <br> <br>
     Producto interno bruto (PIB): 2.913.623 M$`
     fotomostrar = document.querySelector("img")
-    fotomostrar.setAttribute("src", "reino.png")
-
-    campo.animate(
-        {
-          opacity: [0, 1]
-        },
-        2100,
-      );
+    fotomostrar.setAttribute("src", "reino.avif")
 
     document.getElementById("campo").style.display = "block"
     document.getElementById("bandera").style.display = "block"
@@ -217,8 +164,8 @@ botonmostrar.addEventListener("click", mostrarchina)
 
 function mostrarchina() {
     pin.style.display = "none";
-    pin.style.top = "120px";
-    pin.style.left = "210px";
+    pin.style.top = "100px";
+    pin.style.left = "190px";
     var h = plano.animate(
         { transform: "translateX(-640px)" },
         { duration: 800, fill: "forwards" }
@@ -232,21 +179,11 @@ function mostrarchina() {
 
     texto = document.querySelector("p")
     texto.innerHTML = `Presidente: Xi Jinping.<br><br>
-    Capital: Pekín. <br> <br>
     Platos típicos: El pato Pekinés, Sopa Wan Tan, Zongzi.<br><br>
     Lugares turísticos: Gran muralla, la Ciudad Prohibida en Beijing, las Montañas de Guilin.<br><br>
-    Población: 1,412 miles de millones de personas. <br> <br>
-    Extensión territorial: 9,597 millones km². <br> <br>
     Producto interno bruto (PIB): 17.174.722 M$`
 
     fotomostrar.setAttribute("src", "china.jpeg")
-
-    campo.animate(
-        {
-          opacity: [0, 1]
-        },
-        2100,
-      );
 
     document.getElementById("campo").style.display = "block"
     document.getElementById("bandera").style.display = "block"
@@ -262,8 +199,8 @@ botonmostrar.addEventListener("click", mostrarindia)
 
 function mostrarindia() {
     pin.style.display = "none";
-    pin.style.top = "180px";
-    pin.style.left = "110px";
+    pin.style.top = "160px";
+    pin.style.left = "90px";
     var h = plano.animate(
         { transform: "translateX(-640px)" },
         { duration: 800, fill: "forwards" }
@@ -277,21 +214,12 @@ function mostrarindia() {
 
     texto = document.querySelector("p")
     texto.innerHTML = `Presidente: Draupadi Murmu.<br><br>
-    Capital: Nueva Delhi. <br> <br>
     Platos típicos: Samosas, Biryani, Pollo tikka masala.<br><br>
     Lugares turísticos: Taj Mahal, Templo Dorado, Fuerte Amber.<br><br>
-    Población: 1,408 miles de millones km² de personas. <br> <br>
-    Extensión territorial: 9,597 millones km². <br> <br>
     Producto interno bruto (PIB): 2.661.796 M$`
 
-    fotomostrar.setAttribute("src", "india.png")
 
-    campo.animate(
-        {
-          opacity: [0, 1]
-        },
-        2100,
-      );
+    fotomostrar.setAttribute("src", "india.png")
 
     document.getElementById("campo").style.display = "block"
     document.getElementById("bandera").style.display = "block"
@@ -306,10 +234,9 @@ botonmostrar = document.getElementById("pais7")
 botonmostrar.addEventListener("click", mostrarsudafrica)
 
 function mostrarsudafrica() {
-
     pin.style.display = "none";
-    pin.style.top = "340px";
-    pin.style.left = "200px";
+    pin.style.top = "320px";
+    pin.style.left = "180px";
     var h = plano.animate(
         { transform: "translateX(-380px)" },
         { duration: 800, fill: "forwards" }
@@ -323,28 +250,17 @@ function mostrarsudafrica() {
 
     texto = document.querySelector("p")
     texto.innerHTML = `Presidente: Cyril Ramaphosa.<br><br>
-    Capital: Pretoria (capital administrativa), Ciudad del Cabo (capital legislativa) y Bloemfontein (capital judicial). <br> <br>
     Platos típicos: Amasi, Biltong, Bobotie.<br><br>
     Lugares turísticos: Parque nacional Kruger, Ciudad del Cabo, Johannesburgo.<br><br>
-    Población: 59,39 millones km² de personas. <br> <br>
-    Extensión territorial: 1,22 millones km². <br> <br>
     Producto interno bruto (PIB): 384.964 M$`
+
 
     fotomostrar.setAttribute("src", "sudafrica.png")
 
-    campo.animate(
-        {
-          opacity: [0, 1]
-        },
-        2100,
-      );
-
     document.getElementById("campo").style.display = "block"
     document.getElementById("bandera").style.display = "block"
-
     document.getElementById("regresar").style.display = "block"
     document.getElementById("textoinicio").style.display = "none"
-
 }
 
 //ELEMENTOS PARA MOSTRAR EL CONTENIDO DE MARRUECOS
@@ -355,8 +271,8 @@ botonmostrar.addEventListener("click", mostrarmarruecos)
 
 function mostrarmarruecos() {
     pin.style.display = "none";
-    pin.style.top = "160px";
-    pin.style.left = "85px";
+    pin.style.top = "150px";
+    pin.style.left = "65px";
     var h = plano.animate(
         { transform: "translateX(-380px)" },
         { duration: 800, fill: "forwards" }
@@ -370,21 +286,12 @@ function mostrarmarruecos() {
 
     texto = document.querySelector("p")
     texto.innerHTML = `Presidente: Mohamed VI.<br><br>
-    Capital: Rabat. <br> <br>
     Platos típicos: Tajines, Cuscús con verduras, Kefta.<br><br>
     Lugares turísticos: Marrakech, Mezquita Hassan II, Palacio de la bahía.<br><br>
-    Población: 37,08 millones km² de personas. <br> <br>
-    Extensión territorial: 710.850 km². <br> <br>
     Producto interno bruto (PIB): 127.596 M$`
 
-    fotomostrar.setAttribute("src", "marruecos.png")
 
-    campo.animate(
-        {
-          opacity: [0, 1]
-        },
-        2100,
-      );
+    fotomostrar.setAttribute("src", "marruecos.png")
 
     document.getElementById("campo").style.display = "block"
     document.getElementById("bandera").style.display = "block"
@@ -412,33 +319,20 @@ function mostraraustralia() {
 
     titulo.innerHTML = "Australia"
 
+
     texto = document.querySelector("p")
     texto.innerHTML = `Presidente: Anthony Albanese.<br><br>
-    Capital: Canberra. <br> <br>
     Platos típicos: meatloaf, Pavlova, hoki.<br><br>
     Lugares turísticos: Ópera de Sídney, Uluru, Gran Barrera de Coral.<br><br>
-    Población: 25,69 millones km² de personas. <br> <br>
-    Extensión territorial: 7,688 millones km². <br> <br>
     Producto interno bruto (PIB): 1.701.893 M$`
 
     fotomostrar = document.querySelector("img")
     fotomostrar.setAttribute("src", "australia.png")
-
-    campo.animate(
-        {
-          opacity: [0, 1]
-        },
-        2100,
-      );
-
     document.getElementById("campo").style.display = "block"
     document.getElementById("bandera").style.display = "block"
     document.getElementById("regresar").style.display = "block"
     document.getElementById("textoinicio").style.display = "none"
-
 }
-
-
 
 //ELEMENTOS PARA MOSTRAR EL CONTENIDO DE NUEVA ZELANDA
 
@@ -448,8 +342,8 @@ botonmostrar.addEventListener("click", mostrarnv)
 
 function mostrarnv() {
     pin.style.display = "none";
-    pin.style.top = "375px";
-    pin.style.left = "270px";
+    pin.style.top = "345px";
+    pin.style.left = "260px";
     var h = plano.animate(
         { transform: "translateX(-760px)" },
         { duration: 800, fill: "forwards" }
@@ -462,22 +356,12 @@ function mostrarnv() {
 
     texto = document.querySelector("p")
     texto.innerHTML = `Presidente: Chris Hipkins.<br><br>
-    Capital: Wellington. <br> <br>
     Platos típicos: Hangi maorí, Asado Hogget, teririki.<br><br>
     Lugares turísticos: Auckland, Queenstown, Wellington.<br><br>
-    Población: 5,123 millones km² de personas. <br> <br>
-    Extensión territorial: 268.021 km². <br> <br>
     Producto interno bruto (PIB): 233.016 M$`
 
+
     fotomostrar.setAttribute("src", "nuevazelanda.png")
-
-    campo.animate(
-        {
-          opacity: [0, 1]
-        },
-        2100,
-      );
-
     document.getElementById("campo").style.display = "block"
     document.getElementById("bandera").style.display = "block"
     document.getElementById("regresar").style.display = "block"
@@ -489,29 +373,30 @@ function mostrarnv() {
 
 botonmostrar = document.getElementById("regresar")
 botonmostrar.addEventListener("click", mostrarve)
+
 function mostrarve() {
 
+    pin.style.display = "none";
+    pin.style.top = "200px";
+    pin.style.left = "260px";
+    var h = plano.animate(
+        { transform: "translateX(0px)" },
+        { duration: 800, fill: "forwards" }
+    )
+    h.onfinish = function () {
+        pin.style.display = "block";
+    };
     titulo = document.querySelector("#nom-pais")
     titulo.innerHTML = "Venezuela"
 
     texto = document.querySelector("p")
     texto.innerHTML = `Presidente: Nicolás Maduro.<br><br>
-     Capital: Caracas. <br> <br>
      Platos típicos: arepa, pabellón criollo, cachapa.<br><br>
      Lugares turísticos: Parque Nacional Morrocoy, Archipiélago Los Roques, Colonia Tovar.<br><br>
-     Población: 28,2 millones km² de personas. <br> <br>
-     Extensión territorial: 916.445 km². <br> <br>
      Producto interno bruto (PIB): 113.055 M€`
 
     fotomostrar = document.querySelector("img")
     fotomostrar.setAttribute("src", "venezuela.png")
-
-    campo.animate(
-        {
-          opacity: [0, 1]
-        },
-        2100,
-      );
 
 
     document.getElementById("campo").style.display = "block"
